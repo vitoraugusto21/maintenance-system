05c76b569c5ffd12b08b0f525d1f92c2fcfd2ce3
Gabriel Vitor <gabrielvitortec@gmail.com>
2023-04-05 13:31:00 -0300

    Update ManagerDAO.java

--- src/Model/Dao/ManagerDAO.java
+++ src/Model/Dao/ManagerDAO.java
@@ -1,6 +1,17 @@
 package Model.Dao;
 
 public interface ManagerDAO {
+
+    void insertManager(Manager manager);
+
+    void updateManager(Manager manager, String attributeToChange, String newAttribute);
+
+    void deleteManager(Manager manager);
+
+    ArrayList<Manager> getAllManagers();
+
+    Manager getManagerById(String id);
+
     public void manageStock();
     public void viewReports();
 }
