b8c06f7bcb6f2161f74e27899cff72f03160041a
Vítor Augusto <vitoraug210@gmail.com>
2023-04-06 16:09:46 -0300

    Update OsDao and OsDaoImp

--- src/Model/Dao/OsDAO.java
+++ src/Model/Dao/OsDAO.java
@@ -2,14 +2,33 @@ package Model.Dao;
 
 import Model.entities.Invoice;
 import Model.entities.Os;
+import Model.entities.Product;
+import Model.entities.Technician;
 import Model.entities.enums.OsStatus;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public interface OsDAO {
-    public void createOs(Os os);
-    public void updateOs(Os os);
-    public void deleteOs(Os os);
+    public void assignOs(Os os, ArrayList clientOs);
+
+    public void deleteOs(Os os, ArrayList clientOs);
+
+    public void assignEndTime(Os os, Date endTime);
+
+    public void cleaning(Os os);
+
+    public void addOperationalSystem(Os os);
+
+    public void addPrograms(Os os, int quantity);
+
+    public void addParts(Os os, Product part);
+
+    public void updateStatus(Os os, OsStatus status);
+
+    public void addDuration(Os os);
+
+    public void addTechnician(Os os, Technician technician);
     public void viewOs(String osId);
     public void genReport(String osId);
     public void genInvoice(Invoice invoice);
--- src/Model/Dao/OsDAOImp.java
+++ src/Model/Dao/OsDAOImp.java
@@ -1,22 +1,55 @@
 package Model.Dao;
 
-import Model.entities.Invoice;
-import Model.entities.Os;
+import Model.entities.*;
 import Model.entities.enums.OsStatus;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class OsDAOImp implements OsDAO{
 
     public OsDAOImp(){};
+    public void assignOs(Os os, ArrayList clientOs){ //adicionar os ao registro
+        clientOs.add(os);
+    };
+     
+    public void deleteOs(Os os, ArrayList clientOs){ //deletar os do registro
+        clientOs.remove(os);
+    };
 
-    public List<Os> osList(){
-        return osList();
+    public void assignEndTime(Os os, Date endTime){
+        os.setEndTime(endTime);
     }
-    public void createOs(Os os){};
-    public void updateOs(Os os){};
-    public void deleteOs(Os os){};
+
+    public void cleaning(Os os){ //adicionar limpeza a OS
+        Double totalValue = os.getTotalValue() + os.getCleaning();
+        os.setTotalValue(totalValue);
+    }
+
+    public void addOperationalSystem(Os os){
+        Double totalValue = os.getTotalValue() + os.getOperationalSystem();
+        os.setTotalValue(totalValue);
+    }
+
+    public void addPrograms(Os os, int quantity){ //adicionar os programas (e quantos) a os
+        Double totalValue = os.getTotalValue() + (os.getPrograms() * quantity);
+        os.setTotalValue(totalValue);
+    }
+
+    public void addParts(Os os, Product part){
+        os.getUsedParts().add(part);
+        Double totalValue = os.getTotalValue() + part.getProductPrice();
+    }
+
+    public void updateStatus(Os os, OsStatus status){} //Mudar o status da Os
+
+    public void addDuration(Os os){}; //adicionar qual foi o tempo de duração da OS
+
+    public void addTechnician(Os os, Technician technician){ //adicionar id do tecnico que pegou a os
+        os.setTechnicianId(technician.getId());
+    }
+
     public void viewOs(String osId){};
     public void genReport(String osId){};
     public void genInvoice(Invoice invoice){};
