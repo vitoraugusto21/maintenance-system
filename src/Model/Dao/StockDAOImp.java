b0c5662d516fe4b3ce931af298c298d9bac901e2
Gabriel Vitor <gabrielvitortec@gmail.com>
2023-04-09 19:04:56 -0300

    Update StockDAOImp.java

--- src/Model/Dao/StockDAOImp.java
+++ src/Model/Dao/StockDAOImp.java
@@ -1,16 +1,28 @@
 package Model.Dao;
 
+import Model.entities.Client;
 import Model.entities.Product;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 public class StockDAOImp implements StockDAO{
 
-    public StockDAOImp(){};
+    public StockDAOImp(){
+        
+    };
+
+    private final Map<String, Product> stock = new HashMap<>();
 
     public List<Product> productList(){
         return productList();
     }
-    public void addProduct(Product product){};
-    public void removerProduto(Product product){};
+    public void addProduct(Product product){
+
+    };
+    public void removerProduto(Product product){
+
+    };
 }
