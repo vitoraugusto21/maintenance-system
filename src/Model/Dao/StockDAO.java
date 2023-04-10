a2f8d48d7921d695bcade509f0fcb3f5bf290325
Gabriel Vitor <gabrielvitortec@gmail.com>
2023-04-09 21:41:05 -0300

    Update stockDAO and StockDAOImp

--- src/Model/Dao/StockDAO.java
+++ src/Model/Dao/StockDAO.java
@@ -1,10 +1,14 @@
 package Model.Dao;
 
+import Model.entities.Client;
 import Model.entities.Product;
+
+import java.util.ArrayList;
 
 public interface StockDAO {
     public void insertProduct(Product product);
     public void deleteProduct(Product product);
 
+    ArrayList<Product> getAllProducts();
 
 }
--- src/Model/Dao/StockDAOImp.java
+++ src/Model/Dao/StockDAOImp.java
@@ -3,6 +3,7 @@ package Model.Dao;
 import Model.entities.Client;
 import Model.entities.Product;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,20 +13,19 @@ public class StockDAOImp implements StockDAO{
 
     public StockDAOImp(){
 
-    };
+    }
 
     private final Map<String, Product> stock = new HashMap<>();
 
-    public List<Product> productList(){
-
-        return productList();
-    }
-
-    @Override
     public void insertProduct(Product product) {
         stock.put(product.getProductCode(), product);
     }
-    public void removerProduto(Product product){
+    public void deleteProduct(Product product){
+        stock.remove(product.getProductCode());
+    }
 
+    @Override
+    public ArrayList<Product> getAllProducts() {
+        return new ArrayList<>(stock.values());
     }
 }
