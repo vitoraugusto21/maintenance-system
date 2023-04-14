dd4d5b4bb63f45b5c896a8e88622f6b46a7c9fd3
Vítor Augusto <vitoraug210@gmail.com>
2023-04-14 18:04:53 -0300

    Update StockDAOImpTest
    Adicionei o teste de insertProduct na classe.

--- src/test/StockDAOImpTest.java
+++ src/test/StockDAOImpTest.java
@@ -1,4 +1,28 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import Model.Dao.StockDAOImp;
+import Model.entities.Product;
+import Model.entities.Stock;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+
 public class StockDAOImpTest {
+
+    @Test
+    public void insertProductTest(){
+        Product product = new Product();
+        Stock stock = new Stock();
+        StockDAOImp stockDAOImp = new StockDAOImp();
+        ArrayList<Product> stockProducts = new ArrayList<>();
+        stock.setListProducts(stockProducts);
+
+        stockDAOImp.insertProduct(product, stock.getListProducts());
+
+        assertTrue(stock.getListProducts().contains(product));
+    }
+
+
 }
