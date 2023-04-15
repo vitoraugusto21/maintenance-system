9318876ed95796a671a920defac45b98e6491536
Gabriel Vitor <gabrielvitortec@gmail.com>
2023-04-15 18:37:56 -0300

    Update Stock.java
    Colocando Javadoc na classe Stock.java

--- src/Model/entities/Stock.java
+++ src/Model/entities/Stock.java
@@ -2,6 +2,11 @@ package Model.entities;
 
 import java.util.ArrayList;
 
+/**
+ * Classe que representa o estoque de produtos.
+ * Herda as propriedades da classe Product.
+ * @author Vitor Augusto, Gabriel Vitor
+ */
 public class Stock extends Product{
     private ArrayList<Product> listProducts;
 
@@ -9,10 +14,22 @@ public class Stock extends Product{
         return listProducts;
     }
 
+    /**
+     * Define a lista de produtos do estoque.
+     * @param listProducts - lista de produtos
+     */
     public void setListProducts(ArrayList<Product> listProducts) {
         this.listProducts = listProducts;
     }
 
+    /**
+     * Construtor com argumentos.
+     * @param productCode - código ou id do produto
+     * @param productName - nome do produto
+     * @param productPrice - preço do produto
+     * @param productQuantity - quantidade do produto
+     * @param listProducts - lista de produtos
+     */
     public Stock(String productCode, String productName, Double productPrice, Integer productQuantity, ArrayList<Product> listProducts) {
         super(productCode, productName, productPrice, productQuantity);
         this.listProducts = listProducts;
