9bf0f176e918e4d2162afbe51eda52512ccdd314
Gabriel Vitor <gabrielvitortec@gmail.com>
2023-04-15 18:26:00 -0300

    Update invoice.java with javadoc
    Adicionando javadoc em Invoice e colocando os autores

--- src/Model/entities/Invoice.java
+++ src/Model/entities/Invoice.java
@@ -2,6 +2,11 @@ package Model.entities;
 
 import java.util.Date;
 
+/**
+ * Classe que representa uma fatura emitida pela empresa para um cliente
+ * com as informações dos produtos comprados e os valores
+ * @author Vitor Augusto, Gabriel Vitor
+ */
 public class Invoice {
     private String idInvoice;
     private String nameCompany;
