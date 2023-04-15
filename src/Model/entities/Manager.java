ded6819c312661820de068d47d3ec764bb0a7253
Gabriel Vitor <gabrielvitortec@gmail.com>
2023-04-15 12:32:30 -0300

    Update Manager.java

--- src/Model/entities/Manager.java
+++ src/Model/entities/Manager.java
@@ -2,9 +2,9 @@ package Model.entities;
 
 public class Manager extends Person{
     private String mPassword;
-    public Manager(String id, String name, String phoneNumber, String email, String adress, String Mpassword) {
-        super(id, name, phoneNumber, email, adress);
-        mPassword = mPassword;
+    public Manager(String id, String name, String phoneNumber, String email, String address, String mPassword) {
+        super(id, name, phoneNumber, email, address);
+        this.mPassword = mPassword;
     }
 
     public String getPassword() {
