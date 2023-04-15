38abf9051dd3ca44f058e4390205316af2b70feb
Gabriel Vitor <gabrielvitortec@gmail.com>
2023-04-15 12:25:27 -0300

    Update Client.java

--- src/Model/entities/Client.java
+++ src/Model/entities/Client.java
@@ -5,19 +5,37 @@ import Model.Dao.ClientDAO;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A classe Client é responsável por representar um cliente e suas informações no sistema. É uma subclasse da classe Person.
+ * @author Vitor Augusto, Gabriel Vitor
+ */
 public  class Client extends Person{
 
     private ArrayList<Os> clientOs; //lista de ordens de serviços dos clientes
 
+
+    /**
+     * Construtor padrão da classe Client.
+     */
     public Client() {
     }
 
-    public Client(String id, String name, String phoneNumber, String email, String adress) {
-        super(id, name, phoneNumber, email, adress);
+
+    /**
+     * Construtor com argumentos da classe Client.
+     *
+     * @param id o id do cliente.
+     * @param name o nome do cliente.
+     * @param phoneNumber o número de telefone do cliente.
+     * @param email o email do cliente.
+     * @param address o endereço do cliente.
+     */
+    public Client(String id, String name, String phoneNumber, String email, String address) {
+        super(id, name, phoneNumber, email, address);
     }
 
-    public Client(String id, String name, String phoneNumber, String email, String adress, ArrayList<Os> clientOs) {
-        super(id, name, phoneNumber, email, adress);
+    public Client(String id, String name, String phoneNumber, String email, String address, ArrayList<Os> clientOs) {
+        super(id, name, phoneNumber, email, address);
         this.clientOs = clientOs;
     }
 
