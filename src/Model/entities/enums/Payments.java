09712876b6c461753f7532cce9689d7329008efc
Gabriel Vitor <gabrielvitortec@gmail.com>
2023-04-15 18:46:49 -0300

    Update Payments.java
    adicionando javadoc

--- src/Model/entities/enums/Payments.java
+++ src/Model/entities/enums/Payments.java
@@ -1,5 +1,9 @@
 package Model.entities.enums;
 
+/**
+ * Enumeração dos tipos de pagamentos disponíveis.
+ * Possui os seguintes tipos de pagamentos:
+ */
 public enum Payments {
     CREDIT,
     DEBIT,
