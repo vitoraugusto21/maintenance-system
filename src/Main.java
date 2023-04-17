1dfa4606b875c79846267bbcba7f8ad591779299
Vítor Augusto <vitoraug210@gmail.com>
2023-04-17 10:51:34 -0300

    Update Main.java
    Descartei os trechos de codigo da main

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,6 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-
-        Queue<Os> queue = new LinkedList<>();
-        OsDAOImp dao = new OsDAOImp();
-        Technician tec = new Technician("1", "John", "john@test.com", "123456", "1234");
-
-        queue.add(new Os("1"));
-        queue.add(new Os("2"));
-        queue.add(new Os("3"));
-        queue.add(new Os("4"));
-
-        dao.takeOs(queue,tec);
-
-        System.out.println(queue);
     }
 }
 
