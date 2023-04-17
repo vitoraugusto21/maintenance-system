a4c9bf7956a445e1ce1eed7ba4d8ac4a00522f48
Vítor Augusto <vitoraug210@gmail.com>
2023-04-17 11:56:34 -0300

    Update InvoiceDAO.java
    Adicionei os metodos  updateInvoiceAttendant
    updateInvoiceAttendant
    updateInvoiceClient

--- src/Model/Dao/InvoiceDAO.java
+++ src/Model/Dao/InvoiceDAO.java
@@ -1,13 +1,19 @@
 package Model.Dao;
 
+import Model.entities.Attendant;
 import Model.entities.Client;
 import Model.entities.Invoice;
+import Model.entities.Technician;
 
 import java.util.ArrayList;
 
 public interface InvoiceDAO{
     public void insertInvoice(Invoice invoice);
     public void updateInvoiceClient(Invoice invoice, Client client);
+    public void updateInvoiceAttendant(Invoice invoice, Attendant attendant);
+    public void updateInvoiceClient(Invoice invoice, Technician technician);
+
+
     public void deleteInvoice(Invoice invoice);
     public Invoice viewInvoice(Invoice invoice);
     ArrayList<Invoice> getAllInvoices();
