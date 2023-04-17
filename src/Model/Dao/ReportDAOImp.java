d5a001ec0376ddad665d56ccb6dab5a550b53be7
Vítor Augusto <vitoraug210@gmail.com>
2023-04-17 12:40:54 -0300

    Create ReportDAOImp.java

---
+++ src/Model/Dao/ReportDAOImp.java
@@ -0,0 +1,29 @@
+package Model.Dao;
+
+import Model.entities.Os;
+import Model.entities.Report;
+
+public class ReportDAOImp implements ReportDAO{
+    public ReportDAOImp(){};
+
+    @Override
+    public void createReport(Report report) {
+        
+    }
+
+    @Override
+    public void deleteReport(Report report) {
+
+    }
+
+    @Override
+    public void updateReport(Report report, Os os) {
+
+    }
+
+    @Override
+    public Report genReport() {
+        return null;
+    }
+
+}
