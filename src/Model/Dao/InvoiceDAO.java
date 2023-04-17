8c1f7417c3451bd2a6bc904eff43038d751bc67f
Vítor Augusto <vitoraug210@gmail.com>
2023-04-17 11:50:42 -0300

    Update InvoiceDAO.java
    Troquei o metodo viewInvoice de void para invoice

--- src/Model/Dao/InvoiceDAO.java
+++ src/Model/Dao/InvoiceDAO.java
@@ -9,6 +9,6 @@ public interface InvoiceDAO{
     public void insertInvoice(Invoice invoice);
     //public void updateInvoice(Invoice invoice, String attributteToChange, String newAttributte);
     public void deleteInvoice(Invoice invoice);
-    public void viewInvoice(Invoice invoice);
+    public Invoice viewInvoice(Invoice invoice);
     ArrayList<Invoice> getAllInvoices();
 }
