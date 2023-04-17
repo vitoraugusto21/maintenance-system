aaa12033e078b4115f3d3c1177bf95fca8217ded
Vítor Augusto <vitoraug210@gmail.com>
2023-04-17 11:39:49 -0300

    Update InvoiceDAO.java
    Alterei o metodo insertInvoice

--- src/Model/Dao/InvoiceDAO.java
+++ src/Model/Dao/InvoiceDAO.java
@@ -6,8 +6,8 @@ import Model.entities.Invoice;
 import java.util.ArrayList;
 
 public interface InvoiceDAO{
-    public void insertInvoice(Invoice invoice, ArrayList invoicesList);
-    public void updateInvoice(Invoice invoice, String attributteToChange, String newAttributte);
+    public void insertInvoice(Invoice invoice);
+    //public void updateInvoice(Invoice invoice, String attributteToChange, String newAttributte);
     public void deleteInvoice(Invoice invoice);
     public void getInvoice(Invoice invoice);
     ArrayList<Invoice> getAllInvoices();
