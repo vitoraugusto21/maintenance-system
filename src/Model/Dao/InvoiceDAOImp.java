2c8d660801ff4fbe4bfddb7eecde57cfb80e863f
Vítor Augusto <vitoraug210@gmail.com>
2023-04-16 21:02:29 -0300

    Create InvoiceDAOImp.java
    Criei a classe InvoiceDAOImp.java.

---
+++ src/Model/Dao/InvoiceDAOImp.java
@@ -0,0 +1,35 @@
+package Model.Dao;
+
+import Model.entities.Invoice;
+
+import java.util.ArrayList;
+
+public class InvoiceDAOImp implements InvoiceDAO{
+
+    public InvoiceDAOImp(){};
+
+    @Override
+    public void insertInvoice(Invoice invoice, ArrayList invoicesList) {
+
+    }
+
+    @Override
+    public void updateInvoice(Invoice invoice, String attributteToChange, String newAttributte) {
+
+    }
+
+    @Override
+    public void deleteInvoice(Invoice invoice) {
+
+    }
+
+    @Override
+    public void getInvoice(Invoice invoice) {
+
+    }
+
+    @Override
+    public ArrayList<Invoice> getAllInvoices() {
+        return null;
+    }
+}
