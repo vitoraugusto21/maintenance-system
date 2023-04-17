e43f25e49911c151d8f80db1b60b5680c1605116
Vítor Augusto <vitoraug210@gmail.com>
2023-04-16 21:22:26 -0300

    Update InvoiceDAOImp

--- src/Model/Dao/InvoiceDAOImp.java
+++ src/Model/Dao/InvoiceDAOImp.java
@@ -10,12 +10,18 @@ public class InvoiceDAOImp implements InvoiceDAO{
 
     @Override
     public void insertInvoice(Invoice invoice, ArrayList invoicesList) {
-
+        invoicesList.add(invoice);
     }
 
     @Override
     public void updateInvoice(Invoice invoice, String attributteToChange, String newAttributte) {
-
+        switch (attributteToChange.toLowerCase()) {
+            case "client" -> invoice.setClient(newAttributte);
+            case "technician" -> invoice.setTechnician(newAttributte);
+            case "attendant" -> invoice.setAttendant(newAttributte);
+            case "os" -> invoice.setOs(newAttributte);
+            default -> throw new IllegalArgumentException("Invalid attribute name");
+        }
     }
 
     @Override
