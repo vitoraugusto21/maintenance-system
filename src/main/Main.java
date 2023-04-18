91c6506a066f4e38b4abe581546158ea2958f56c
Gabriel Vitor <gabrielvitortec@gmail.com>
2023-04-17 23:58:10 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,14 @@
 package main;
 
+import main.model.dao.OsDAOImp;
+import main.model.dao.ReportDAOImp;
 import main.model.entities.*;
 import main.model.entities.enums.Payments;
 
 import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         var client = new Client();
         Attendant attendant1 = new Attendant("001", "John Doe", "555-1234", "john.doe@example.com", "123 Main St", "password123");
         Attendant attendant2 = new Attendant("002", "Jane Smith", "555-5678", "jane.smith@example.com", "456 Elm St", "mypassword");
@@ -17,7 +19,14 @@ public class Main {
         Os os = new Os("001", "Repair PC", startTime, Payments.PIX, "001", "100");
 
         // Add os to technician
-        technician.setOs(os);
+        var osDAO = new OsDAOImp();
+        osDAO.insertOs(os);
+        osDAO.takeOs(technician);
+        osDAO.finishOs(technician);
+
+        var report = new Report(os);
+        var reportDAO = new ReportDAOImp();
+        reportDAO.saveReport(report);
     }
 }
 
