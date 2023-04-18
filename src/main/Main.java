dcf92c9a7c25f7f86b08961c790f7a3dd81d7623
Gabriel Vitor <gabrielvitortec@gmail.com>
2023-04-18 00:02:21 -0300

    Update Main.java ortografia

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,10 +10,10 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) throws Exception {
         var client = new Client();
-        Attendant attendant1 = new Attendant("001", "John Doe", "555-1234", "john.doe@example.com", "123 Main St", "password123");
-        Attendant attendant2 = new Attendant("002", "Jane Smith", "555-5678", "jane.smith@example.com", "456 Elm St", "mypassword");
+        Attendant attendant1 = new Attendant("001", "John", "555-1234", "john@example.com", "123 Main St", "password123");
+        Attendant attendant2 = new Attendant("002", "Jane", "555-5678", "jane@example.com", "456 Elm St", "mypassword");
         Manager manager = new Manager("001", "John Doe", "555-1234", "john.doe@example.com", "123 Main St", "password123");
-        Technician technician = new Technician("001", "Jane Doe", "555-1234", "jane.doe@example.com", "123 Main St", "password123");
+        Technician technician = new Technician("001", "Jano", "555-1234", "jano@example.com", "123 Main St", "password123");
 
         Date startTime = new Date();
         Os os = new Os("001", "Repair PC", startTime, Payments.PIX, "001", "100");
